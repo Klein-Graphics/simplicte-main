55bd30a0eb93ab9761343c2cf248984fd7a86fac
Will Leonardi <wleonardi@kleingraphics.net>
2012-04-27 08:45:07 +0000

    Added docBlock's to Page_Loading

--- sc/core/libraries/Page_loading.php
+++ sc/core/libraries/Page_loading.php
@@ -1,122 +1,186 @@
 <?php
 
-  //----------------------
-  //Page Loading Functions
-  //----------------------
-  //
-  // These functions handle the loading of simplecart onto the user's page
-  //
-  
-  namespace Library;
-  
-  class Page_loading extends \SC_Library {
-  
+/**
+ * Page Loading Library
+ *
+ * Contains the display parsing for simplecart, including the tag replacement and
+ * javascript insertion
+ *
+ * @package Display
+ */  
+
+namespace Library;
+
+/**
+ * Page Loading library class
+ *
+ * @package Display
+ */
+class Page_loading extends \SC_Library {
+    
+    /**
+     * Construct
+     * 
+     * Initializes the Javascript and Loaded Drivers variables, then loads the
+     * display drivers and initalizes the item templates
+     *
+     * @return null
+     */
     function __construct() {
-      parent::__construct();
-      $this->js = array();
-      
-      $this->loaded_drivers = array();
-      $this->load_drivers();
-      $this->initialize_item_templates();
-      
+        parent::__construct();
+        $this->js = array();
+
+        $this->loaded_drivers = array();
+        $this->load_drivers();
+        $this->initialize_item_templates();
+
     }
-  
+
+    /**
+     * Replace Tag
+     *
+     * Replaces a short tag with the desired html or data
+     *
+     * @return string
+     *
+     * @param string $input The input to search for the tag
+     * @param string|array $tag Either a string containing the tag to replace
+     * or an associate array, with the keys being the tags to replace and the
+     * value being what to replace them with
+     * @param string|callback $replacement A string containing what to replace
+     * the tag with, or a callback function that is fed the attributes
+     * that are appended to the tag, and whose return value is used to replace
+     * the tag
+     */
     function replace_tag($input,$tag,$replacement=NULL) {  
-    
-      if (is_array($tag)) {
-        foreach($tag as $name => $content) {
-            $input = $this->replace_tag($input,$name,$content);
+
+        if (is_array($tag)) {
+            foreach($tag as $name => $content) {
+                $input = $this->replace_tag($input,$name,$content);
+            }
+
+            return $input;
         }
-        
+
+        //Find the tag in the page
+        if (preg_match_all("/\[\[($tag(\|[^(\]\])]+)*)\]\]/i",$input,$matches)) {
+            if (is_callable($replacement)) {
+                $function = $replacement;
+                $replacement = array();
+
+                foreach($matches[1] as $key => $match) {
+                    $match = explode('|',$match);
+
+                    foreach ($match as &$this_match) {
+                        if (strpos($this_match,'i--') !== FALSE) {
+                            $this_match = substr($this_match,3);
+                          
+                            $item = \Model\Item::find('first',array(
+                                'conditions' => array('number = ?',$this_match))
+                            ); 
+                                
+                            $this_match = $item->id;      
+                        }    
+                    } 
+
+                    $replacement[$key] = $function($match);
+                }
+            }              
+
+            return str_replace($matches[0],$replacement,$input);
+
+        }
+
         return $input;
-      }
-      
-      //Find the tag in the page
-      if (preg_match_all("/\[\[($tag(\|[^(\]\])]+)*)\]\]/i",$input,$matches)) {
-        if (is_callable($replacement)) {
-          $function = $replacement;
-          $replacement = array();
-          
-          foreach($matches[1] as $key => $match) {
-            $match = explode('|',$match);
-            
-            foreach ($match as &$this_match) {
-                if (strpos($this_match,'i--') !== FALSE) { //This is simply an db id
-                  $this_match = substr($this_match,3);
-                  
-                  $item = \Model\Item::find('first',array('conditions' => array('number = ?',$this_match))); 
-                        
-                  $this_match = $item->id;      
-                }    
-            } 
-            
-            $replacement[$key] = $function($match);
-          }
-        }              
-        
-        return str_replace($matches[0],$replacement,$input);
-      
-      }
-      
-      return $input;
-    
+
     }
-    
+
+    /**
+     * Replace Button
+     *
+     * Specialized function to automatically replace button tags with their correct
+     * image and class
+     *
+     * @return string
+     * 
+     * @param string $input The input to search for the tag
+     * @param string|string[] $tag The tag to replace
+     * @param string Which button to replace the tag with
+     */
     function replace_button($input,$tag,$button) {
-      
-      $readable = str_replace('_',' ',ucfirst($button));
-    
-      return $this->replace_tag($input,$tag,'
+
+        $readable = str_replace('_',' ',ucfirst($button));
+
+        return $this->replace_tag($input,$tag,'
                 <a href="'.sc_ajax($button).'" title="'.$readable.'" class="sc_'.$button.'">
                     <img src="'.sc_asset('button',$button).'" alt="'.$readable.'" />
                 </a>'
             );
     }
-    
+
+    /**
+     * Replace Details
+     *
+     * Replaces all Detail tags with their respective data. Also contains
+     * aliases for the depreciated 'name' and 'desc' tags
+     *
+     * @return string
+     *
+     * @param string $input The input to search for the tags
+     */
     function replace_details($input) {          
-      
-      $tags = array(
+
+        $tags = array(
         'name' => function($args) {
 
             $item = \Model\Item::find($args[1]);          
             return $item->name;        
         },
-        
+
         'desc' => function($args) {
-      
+
             $item = \Model\Item::find($args[1]);          
             return $item->description;      
         },
-        
+
         'detail' => function($args) {
-      
+
             $item = \Model\Item::find($args[2]);          
             return $item->$args[1];         
         }
-      );
-      
-      //Replace "detail" tags.    
-      $input = $this->replace_tag($input,$tags);
-      
-      return $input;
-    
+        );
+
+        //Replace "detail" tags.    
+        $input = $this->replace_tag($input,$tags);
+
+        return $input;
+
     }     
-    
+
+    /**
+     * Initialize Item Templates
+     *
+     * Proccesses and sorts the item templates into their various class
+     * properties
+     *
+     * @return bool Returns false if the template file is missing, otherwise
+     * true
+     */
     function initialize_item_templates() {
         //First, load the item template
         $this->SC->load_library('Config');
         $item_template = $this->SC->Config->get_setting('item_template');
         if (!$item_template) $item_template = 'default';
-        
+
         $template_file = "item_templates/$item_template.html";
-        
+
         $raw_template = file_get_contents("item_templates/$item_template.html");
-        
+
         if (!$raw_template) {
             trigger_error("Item template <strong>$template_file</strong> doesn't exist",E_USER_ERROR);
             return FALSE;
         }
-        
+
         preg_match_all('/<!--TAG (.*?)\s*-->(.*?)<!--ENDTAG-->/s',$raw_template,$tags);      
         foreach ($tags[1] as $key => $tag) {
             $tag = explode(',',$tag);
@@ -124,12 +188,12 @@
                 $this->tags[$this_tag] = $tags[2][$key];
             }
         }
-        
+
         preg_match_all('/<!--OPTION (.*?)\s*-->(.*?)<!--ENDOPTION-->/s',$raw_template,$option_templates);   
         foreach ($option_templates[1] as $key => $tag) {
             $this->option_templates[$tag] = $option_templates[2][$key];
         }  
-        
+
         preg_match_all('/<!--SPECIALTAG (.*?)\s*-->(.*?)<!--ENDSPECIALTAG-->/s',$raw_template,$special_tags);
         foreach ($special_tags[1] as $key => $tag) {
             $tag = explode(' ',$tag);    
@@ -151,25 +215,34 @@
                 'template' => $special_tags[2][$key]
             );
         }                
-        
-        
+
+
         return TRUE;
     }
-    
+
+    /**
+     * Run Item Templates
+     *
+     * Parse the item templates across the input
+     *
+     * @return string
+     *
+     * @param string $input The input to parse
+     */
     function run_item_templates($input) {      
-    
+
         $this->SC->load_library('Items');
-    
+
         $SC = &$this->SC;                                         
-    
+
         $tags = $this->tags;        
-    
+
         $output = $this->replace_tag($input,'item',function($args) use ($tags,$SC) { 
             //Insert the item numbers into the raw template            
             return str_replace('%i',$item,$tags['item']);
         });
         unset($tags['item']);                        
-        
+
         $add_to_cart_callback = function($args) use ($tags,$SC) { 
             //Insert the item numbers into the raw template
             $add_to_cart_code = str_replace('%i',$args[1],$tags['add_to_cart']);  
@@ -202,13 +275,13 @@
             
             return $add_to_cart_code;
         };
-        
+
         $output = $this->replace_tag($input,array(
             'add_to_cart' => $add_to_cart_callback,
             'addtocart' => $add_to_cart_callback //Hack to allow the old tag >:(
         ));
         unset($tags['add_to_cart']);  
-        
+
         if (count($tags)) {
             foreach($tags as $tag => $template) {
                 if ($tag != 'item') {
@@ -218,23 +291,33 @@
                 }
             }                        
         }
-        
+
         $output = $this->replace_tag($output,'options',function($args) use ($SC) {
             return $SC->Page_loading->generate_options_code($args[1]); 
         });
-        
+
         //Run through and replace any special tags
         foreach ($this->special_tags as $special_tag) {     
             $output = preg_replace($special_tag['regex'],$special_tag['template'],$output);
         }        
-        
+
         return $output;                            
     }    
-    
+
+    /**
+     * Generate Options Code
+     *
+     * Generates the options code for a specific item, based on the options template
+     *
+     * @return string
+     *
+     * @param int|string $item Either the item's DB id or it's human readable ID
+     * prefixed with i--
+     */
     function generate_options_code($item) {    
-        
+
         $this->SC->load_library('Items');
-    
+
         if (strpos($item,'i--')!==FALSE) {
             $item_num = substr($item,3);
           
@@ -245,19 +328,19 @@
             
             $item = $item_id->id;
         }        
-    
+
         $item_options = \Model\Itemoption::find('all',array(
             'conditions' => array('itemid = ?',$item)
         ));
-        
+
         $sorted_options = array();                     
-        
+
         foreach ($item_options as $item_option) {
             
             $sorted_options[$item_option->cat][] = $item_option;
         }         
         unset($item_options);
-        
+
         $i = 0;
         foreach ($sorted_options as $cat => $item_options) {
              
@@ -299,29 +382,46 @@
              }
              $i++;
         }
-        
+
         $option_code = '';
-        
+
         foreach ($sorted_options as $sorted_option) {
             $option_code .= $sorted_option['code'].PHP_EOL;
         }              
-        
+
         return $option_code;
-        
           
     }    
-    
-    
+
+    /**
+     * Load Drivers
+     *
+     * Not to be confused with load_driver()
+     *
+     * @see Display/Page_loading/load_driver()
+     *
+     * @return null
+     */
     function load_drivers() {      
-      $this->load_driver(array('cart','checkout','account'));      
+        $this->load_driver(array('cart','checkout','account'));      
     }
-    
+
+    /**
+     * Load Driver
+     *
+     * Loads the specified driver type, checking the Details DB for
+     * which driver to load
+     *
+     * @return bool
+     *
+     * @param string|string[] $type The type of driver to load
+     */
     function load_driver($type) {
-        
+
         if (is_array($type)) {
-        
+
             $return = TRUE;
-        
+
             foreach($type as $this_type) {
                 if (!$this->load_driver($this_type)) {
                     $return = FALSE;
@@ -330,21 +430,21 @@
             
             return $return;
         }
-    
-    
+
+
         $type = strtolower($type);
-    
+
         $driver = $this->SC->Config->get_setting("{$type}_driver");
-        
+
         $driver_parent_file = "core/libraries/{$type}_drivers/".ucfirst($type)."_Driver.php";        
-        
+
         if (file_exists($driver_parent_file)) {
             include_once $driver_parent_file;
         } else {
             trigger_error("Driver type \"$type\" doesn't exist");
             return FALSE;
         }
-        
+
         $driver_file = "core/libraries/{$type}_drivers/$driver.php";        
         if (file_exists($driver_file)) { 
             include_once $driver_file;             
@@ -352,41 +452,61 @@
             trigger_error("Driver \"$driver\" of type \"$type\" doesn't exist");
             return FALSE;
         }
-        
+
         $namespaced_driver = ucfirst($type).'_Driver\\'.$driver;
-        
+
         $driver_object = "{$type}_driver";
-        
+
         $this->$driver_object = new $namespaced_driver;
-        $this->loaded_drivers[$type] = $driver;                
+        $this->loaded_drivers[$type] = $driver;
         
+        return TRUE;                
+
     }
-    
+
+    /**
+     * Add Javascript
+     *
+     * Adds a Javascript file or code
+     *
+     * @param string|array $src A Javascript file, or an array containing the
+     * source at index 0 and raw code at index 1
+     * @param string Raw code to be added
+     */
     function add_javascript($src,$script='') {
-      if (is_array($src)) {
+        if (is_array($src)) {
         $this->js[] = $src;
         return TRUE;
-      }
-      
-      $this->js[] = array($src,$script);
+        }
+
+        $this->js[] = array($src,$script);
     }  
-    
+
+    /**
+     * Insert Javascript
+     *
+     * Inserts Javascript on to the page. 
+     *
+     * @return string
+     *
+     * @param string $input The input to insert the javascript into
+     */
     function insert_javascript($input) {
-    
-      $output = '';
-      
-      if ( ! count($this->js)) {
+
+        $output = '';
+
+        if ( ! count($this->js)) {
         return $input;
-      }
-      
-      foreach ($this->js as $js) {
+        }
+
+        foreach ($this->js as $js) {
         $output .= "<script type='text/javascript' ".(($js[0])?"src='{$js[0]}'":"").">{$js[1]}</script>".PHP_EOL;
-      }
-      $output .= '</head>'.PHP_EOL;
-      
-      
-      return str_ireplace('</head>',$output,$input);
-       
-    }            
-    
-  }
+        }
+        $output .= '</body>'.PHP_EOL;
+
+
+        return str_ireplace('</body>',$output,$input);
+
+        }            
+
+}
